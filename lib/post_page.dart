47ab4e38d3afac5a5190b8f18397361336b5bb37
Mohamed <marcus.jason90@gmail.com>
2022-11-13 19:01:49 +0200

    post and story page

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
---
+++ lib/stories.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Stories extends StatelessWidget {
+  const Stories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
