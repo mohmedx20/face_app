da4294f03bfb38ec50d70b21555705e71cefbbcb
Mohamed <marcus.jason90@gmail.com>
2022-11-23 21:12:32 +0200

    Update post_page.dart
    done

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -7,24 +7,108 @@ class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.symmetric(vertical: 50.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.topLeft,
+                child: const CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(Icons.person),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 10, top: 1),
+                alignment: Alignment.topLeft,
+                color: Colors.white,
+                child: Text(
+                  child,
+                  style: const TextStyle(
+                      fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
           Container(
-            margin: const EdgeInsets.all(5),
             alignment: Alignment.topLeft,
-            child: const CircleAvatar(
-              backgroundColor: Colors.black,
-              child: Icon(Icons.person),
+            padding: const EdgeInsets.only(left: 80),
+            child: const Text('3h'),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding:
+                const EdgeInsets.only(top: 30, right: 280, left: 1, bottom: 50),
+            child: const Text('My Post',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 1, color: Colors.grey))),
+            padding: const EdgeInsets.only(left: 10),
+            child: Row(
+              children: [
+                const Text('100'),
+                Image.asset(
+                  'images/like.jpg',
+                  scale: 5,
+                ),
+                Spacer(),
+                const Text(
+                  ' 100 Comments',
+                  textAlign: TextAlign.end,
+                )
+              ],
             ),
           ),
           Container(
-            padding: const EdgeInsets.only(left: 50, top: 5),
-            alignment: Alignment.topLeft,
-            color: Colors.white,
-            child: Text(
-              child,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 1, color: Colors.grey))),
+            child: Row(
+              children: [
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, elevation: 0),
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'images/singleLike.jpg',
+                      width: 20,
+                      height: 20,
+                    ),
+                    label: const Text('Like',
+                        style: TextStyle(color: Colors.grey))),
+                Spacer(),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, elevation: 0),
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'images/comment.jpg',
+                      width: 20,
+                      height: 20,
+                    ),
+                    label: const Text('Comment',
+                        style: TextStyle(color: Colors.grey))),
+                Spacer(),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white, elevation: 0),
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'images/share.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                    label: const Text(
+                      'Share',
+                      style: TextStyle(color: Colors.grey),
+                    ))
+              ],
             ),
           ),
         ],
